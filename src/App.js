0d57042ca8a5db0094c88f58d7c001be993f0d40
VanJackal <v@njackal.com>
2021-08-31 16:32:46 -0500

    moved some code in app.js to prep for topbar and sidebar

--- src/App.js
+++ src/App.js
@@ -4,28 +4,34 @@ import ItemView from "./ItemView"
 import { Grid } from "@material-ui/core"
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = {articleID:"60a829818fb25cc1440c7928", selected:[]}
+    this.state = { articleID: "60a829818fb25cc1440c7928", selected: [] }
   }
 
   changeArticle = (id) => {
-    this.setState({articleID:id});
+    this.setState({ articleID: id });
   }
   render() {
     return (
       <div style={{ height: '100%' }} className="App">
-        <Grid container style={{ height: "100%" }} direction="column">
-          <Grid item xs={12} style={{ maxHeight: "40%", overflow: "auto" }}>
-            <ListView selected={this.state.articleID} clickFunc={this.changeArticle}/>
-          </Grid>
-          <Grid item xs={12} style={{ maxHeight: "60%", overflow: "auto" }}>
-            <ItemView articleID={this.state.articleID} />
-          </Grid>
-        </Grid>
+        <this.FeedView/>
       </div>
     );
+  }
+
+  FeedView = () => {
+    return (
+      <Grid container style={{ height: "100%" }} direction="column">
+        <Grid item xs={12} style={{ maxHeight: "40%", overflow: "auto" }}>
+          <ListView selected={this.state.articleID} clickFunc={this.changeArticle} />
+        </Grid>
+        <Grid item xs={12} style={{ maxHeight: "60%", overflow: "auto" }}>
+          <ItemView articleID={this.state.articleID} />
+        </Grid>
+      </Grid>
+    )
   }
 }
 
