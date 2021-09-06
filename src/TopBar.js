b7bb071c341f045ef91d80c7ae7a3ac248bf83eb
VanJackal <v@njackal.com>
2021-09-06 13:26:56 -0500

    added Refresh button that fetches new feed items

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,17 +1,23 @@
 import React from "react";
+import axios from "axios";
 import { AppBar, Toolbar, IconButton } from "@material-ui/core"
 import { Refresh } from "@material-ui/icons"
+const config = require("./config.json");
 
 class TopBar extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    refreshFeed = async () => {
+        await axios.post(`${config.API}/articles`,{url:"http://node1.h.njackal.com/feed"})
+    }
+
     render() {
         return (
             <div style={{ height: "3em" ,width : '100%'}}>
                 <Toolbar variant="dense">
-                    <IconButton size="small"><Refresh /></IconButton>
+                    <IconButton size="small" onClick={()=>{this.refreshFeed()}}><Refresh /></IconButton>
                 </Toolbar>
             </div>
         )
