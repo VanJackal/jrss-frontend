4515c1763efa313303b1bd9a03b10c1099743e0e
VanJackal <v@njackal.com>
2021-09-13 17:42:07 -0500

    Added adding feeds

--- src/AddFeedButton.js
+++ src/AddFeedButton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,9 +7,12 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+const config = require("./config.json");
 
 export default function AddFeedButton() {
   const [open, setOpen] = React.useState(false);
+
+  let formInput = {};
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -17,6 +21,21 @@ export default function AddFeedButton() {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleSubmit = () => {
+    let feed = {
+      link:formInput.url.value,
+      title:formInput.title.value,
+      feedid:formInput.id.value,
+      description:formInput.desc.value,
+    }
+    addFeed(feed);
+    handleClose();
+  }
+
+  let addFeed = async (feed) =>{
+    await axios.post(`${config.API}/feeds`,feed);
+  }
 
   return (
     <div>
@@ -31,9 +50,34 @@ export default function AddFeedButton() {
           </DialogContentText>
           <TextField
             autoFocus
+            inputRef={ref => {formInput.url = ref}}
             margin="dense"
             id="name"
             label="Feed URL"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => {formInput.title = ref}}
+            margin="dense"
+            id="name"
+            label="FeedTitle"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => {formInput.id = ref}}
+            margin="dense"
+            id="name"
+            label="FeedID"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => {formInput.desc = ref}}
+            margin="dense"
+            id="name"
+            label="FeedDescription"
             fullWidth
           />
         </DialogContent>
@@ -41,7 +85,7 @@ export default function AddFeedButton() {
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleSubmit} color="primary">
             Add
           </Button>
         </DialogActions>
