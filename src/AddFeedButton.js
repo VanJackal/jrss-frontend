1820d24a5b0df4af1fc851c71edcafc0a040fe54
VanJackal <v@njackal.com>
2021-09-16 22:11:34 -0500

    added better addfeed button functionality

--- src/AddFeedButton.js
+++ src/AddFeedButton.js
@@ -13,6 +13,7 @@ export default function AddFeedButton() {
   const [open, setOpen] = React.useState(false);
 
   let formInput = {};
+  const [infoPresets, setRecievedInfo] = React.useState(null);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -20,21 +21,101 @@ export default function AddFeedButton() {
 
   const handleClose = () => {
     setOpen(false);
+    setRecievedInfo(null);
   };
 
   const handleSubmit = () => {
-    let feed = {
-      link:formInput.url.value,
-      title:formInput.title.value,
-      feedid:formInput.id.value,
-      description:formInput.desc.value,
+    if (infoPresets) {
+      let feed = {
+        link: formInput.url.value,
+        title: formInput.title.value,
+        feedid: formInput.id.value,
+        description: formInput.desc.value,
+      }
+      addFeed(feed);
+      handleClose();
+    } else {
+      getFeedInfo();
     }
-    addFeed(feed);
-    handleClose();
   }
 
-  let addFeed = async (feed) =>{
-    await axios.post(`${config.API}/feeds`,feed);
+  let addFeed = async (feed) => {
+    await axios.post(`${config.API}/feeds`, feed);
+  }
+
+  let getFeedInfo = async () => {
+    axios.post(`${config.API}/util/feeds/info`, { url: formInput.url.value }).then(res => {
+      setRecievedInfo({
+        description:res.data.description,
+        title:res.data.title,
+        url:formInput.url.value
+      });
+      console.log(infoPresets);
+    });
+  }
+
+  let GetDialogContent = () => {
+    if (infoPresets) {
+      console.log(infoPresets)
+      return (
+        <DialogContent>
+          <DialogContentText>
+            Enter a Feed URL
+          </DialogContentText>
+          <TextField
+            autoFocus
+            inputRef={ref => { formInput.url = ref }}
+            margin="dense"
+            id="name"
+            label="Feed URL"
+            value={infoPresets?.url || ""}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => { formInput.title = ref }}
+            margin="dense"
+            id="name"
+            label="FeedTitle"
+            defaultValue={infoPresets?.title || ""}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => { formInput.id = ref }}
+            margin="dense"
+            id="name"
+            label="FeedID"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            inputRef={ref => { formInput.desc = ref }}
+            margin="dense"
+            id="name"
+            label="FeedDescription"
+            defaultValue={infoPresets?.description || "Loading"}
+            fullWidth
+          />
+        </DialogContent>
+      )
+    } else {
+      return (
+        <DialogContent>
+          <DialogContentText>
+            Enter a Feed URL
+          </DialogContentText>
+          <TextField
+            autoFocus
+            inputRef={ref => { formInput.url = ref }}
+            margin="dense"
+            id="name"
+            label="Feed URL"
+            fullWidth
+          />
+        </DialogContent>
+      )
+    }
   }
 
   return (
@@ -44,43 +125,7 @@ export default function AddFeedButton() {
       </Button>
       <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title" maxWidth="sm" fullWidth>
         <DialogTitle id="form-dialog-title">Add Feed</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Enter a Feed URL
-          </DialogContentText>
-          <TextField
-            autoFocus
-            inputRef={ref => {formInput.url = ref}}
-            margin="dense"
-            id="name"
-            label="Feed URL"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            inputRef={ref => {formInput.title = ref}}
-            margin="dense"
-            id="name"
-            label="FeedTitle"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            inputRef={ref => {formInput.id = ref}}
-            margin="dense"
-            id="name"
-            label="FeedID"
-            fullWidth
-          />
-          <TextField
-            autoFocus
-            inputRef={ref => {formInput.desc = ref}}
-            margin="dense"
-            id="name"
-            label="FeedDescription"
-            fullWidth
-          />
-        </DialogContent>
+        <GetDialogContent />
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
