cb7b15c45138098350c5005a0f518faaee2bead0
VanJackal <v@njackal.com>
2021-10-17 10:38:36 -0500

    fixed rerender on each feed selection change

--- src/App.js
+++ src/App.js
@@ -9,44 +9,50 @@ const styles = {
   container: { display: "flex", flexDirection: "column", overflow: "auto", height: "100%", width: "100%" }
 };
 
+let AppView = React.memo(({ feedID, selectFeed, dataAge }) => {
+  return (
+    <Grid container direction="row" style={styles.container}>
+      <Grid item style={{ width: "20%" }}>
+        <FeedsView updated={dataAge} clickFunc={selectFeed} selected={feedID} />
+      </Grid>
+      <Grid item style={{ width: "80%", height: "100%" }}>
+        <FeedView feedID={feedID} dataAge={dataAge}/>
+      </Grid>
+    </Grid>
+  )
+}, (prev, next) => {
+  return prev.feedID === next.feedID;
+})
+
+let FeedView = ({feedID, dataAge}) => {
+  const [articleID, setArticleID] = React.useState(null);
+
+  const selectArticle = React.useCallback(id => {
+    setArticleID(id)
+  }, [])
+  return (
+    <Grid container style={{ height: "100%" }} wrap="nowrap" direction="column">
+      <Grid item xs={12} style={{ overflowY: "scroll", flexBasis: "40%" }}>
+        <ListView updated={dataAge} feedid={feedID} selected={articleID} clickFunc={selectArticle} />
+      </Grid>
+      <Grid item xs={12} style={{ overflow: "auto", flexBasis: "60%" }}>
+        <ItemView articleID={articleID} />
+      </Grid>
+    </Grid>
+  )
+}
+
 function App(props) {
   const [feedID, setFeedID] = React.useState(null);
+  const selectFeed = React.useCallback(id => {
+    setFeedID(id);
+  }, [])
   const [dataAge, setDataAge] = React.useState(new Date());
 
   let updateTime = () => {
     setDataAge(new Date());
   }
 
-  let AppView = () => {
-    return (
-      <Grid container direction="row" style={styles.container}>
-        <Grid item style={{ width: "20%" }}>
-          <FeedsView clickFunc={setFeedID} selected={feedID} />
-        </Grid>
-        <Grid item style={{ width: "80%", height: "100%" }}>
-          <FeedView />
-        </Grid>
-      </Grid>
-    )
-  }
-
-  let FeedView = () => {
-    const [articleID, setArticleID] = React.useState(null);
-
-    const selectArticle = React.useCallback(id => {
-      setArticleID(id)
-    }, [])
-    return (
-      <Grid container style={{ height: "100%" }} wrap="nowrap" direction="column">
-        <Grid item xs={12} style={{ overflowY: "scroll", flexBasis: "40%" }}>
-          <ListView updated={dataAge} feedid={feedID} selected={articleID} clickFunc={selectArticle} />
-        </Grid>
-        <Grid item xs={12} style={{ overflow: "auto", flexBasis: "60%" }}>
-          <ItemView articleID={articleID} />
-        </Grid>
-      </Grid>
-    )
-  }
 
   return (
     <div style={{ height: '100vh', width: '100vw', overflow: "auto" }}>
@@ -55,7 +61,7 @@ function App(props) {
           <TopBar updateFunc={updateTime} feedid={feedID} />
         </Grid>
         <Grid item style={{ height: "calc(100% - 3em)" }}>
-          <AppView />
+          <AppView feedID={feedID} selectFeed={selectFeed} dataAge={dataAge} />
         </Grid>
       </Grid>
     </div>
--- src/FeedsView.js
+++ src/FeedsView.js
@@ -7,6 +7,30 @@ let getData = async () => {
     return await (await axios.get(`${config.API}/feeds`)).data
 }
 
+let Header = () => {
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell>Feed</TableCell>
+            </TableRow>
+        </TableHead>
+    )
+}
+
+const FeedListItem = React.memo(({selected,item,clickFunc}) => {
+    let selState = false;
+    if (selected === item._id) {//check if the cell is selected (read it if it is)
+        selState = true;
+    }
+    return (
+        <TableRow selected={selState} key={item._id}>
+            <TableCell onClick={() => { clickFunc(item._id) }}>{item.title}</TableCell>
+        </TableRow>
+    )
+}, (prev,next) => {
+    return prev.selected === next.selected;
+})
+
 function FeedsView(props) {
     const [rowData, setRowData] = React.useState(null);
     React.useEffect(() => {
@@ -18,52 +42,32 @@ function FeedsView(props) {
             }
         }
         updateData()
-    }, [props.selected]);
+    }, []);
 
     let Body = () => {
-        if (!rowData) {
-            return (
-                <TableBody>
-                    <TableRow><TableCell>Loading...</TableCell></TableRow>
-                </TableBody>
-            )//TODO change the loading to replace the table instead of the the table body
-        } else {
-            return (
-                <TableBody>
-                    {
-                        rowData.map((item) => {
-                            let selState = false;
-                            if (props.selected === item._id) {//check if the cell is selected (read it if it is)
-                                selState = true;
-                            }
-                            return (
-                                <TableRow selected={selState} key={item._id}>
-                                    <TableCell onClick={() => { props.clickFunc(item._id) }}>{item.title || "Loading..."}</TableCell>
-                                </TableRow>
-                            )
-                        })
-                    }
-                </TableBody>
-            )
-        }
-    }
-
-    let Header = () => {
         return (
-            <TableHead>
-                <TableRow>
-                    <TableCell>Feed</TableCell>
-                </TableRow>
-            </TableHead>
+            <TableBody>
+                {
+                    rowData.map((item) => {
+                        return(<FeedListItem key={item._id} selected={props.selected} item={item} clickFunc={props.clickFunc}/>)
+                    })
+                }
+            </TableBody>
         )
     }
 
-    return (
-        <Table size="small">
-            <Header />
-            <Body />
-        </Table>
-    )
+    if (!rowData) {
+        return (<p>Loading...</p>)
+    } else {
+        return (
+            <Table size="small">
+                <Header />
+                <Body />
+            </Table>
+        )
+    }
 }
 
-export default FeedsView;
+export default React.memo(FeedsView,(prev,next) => {
+    return prev.updated === next.updated & prev.selected === next.selected;
+});
